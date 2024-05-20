126f7ff033a0924783def49e7d06328dc548f4ab
fiddymulyadi <232220021@unmuhpnk.ac.id>
2024-05-20 08:00:18 +0700

    Update create.blade.php

--- resources/views/penyewa/create.blade.php
+++ resources/views/penyewa/create.blade.php
@@ -18,54 +18,44 @@
                 </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <form action="{{ route('customer.store') }}" method="POST"  >
-                          @csrf
+                        <form action="{{ route('penyewa.store') }}" method="POST"  >
+                            @csrf
+                    
                             <div class="form-group">
-                                <label for="idCustomer">ID Customer</label>
-                                <input type="text" name="id" class="form-control" id="idCustomer" aria-describedby="emailHelp" placeholder="Masukan ID pengguna">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                @error('id_penyewa')
+                                <label for="namaCustomer">Nama Customer</label>
+                                <input type="text" name="nama_penyewa" class="form-control" id="namaCustomer" placeholder="Masukkan nama pengguna">
+                                @error('nama_penyewa')
                                 <div class="alert alert-danger mt-2">
                                     {{ $message }}
                                 </div>
                                 @enderror
                               </div>
-                            <div class="form-group">
-                              <label for="namaCustomer">Nama Customer</label>
-                              <input type="text" name="email" class="form-control" id="namaCustomer" placeholder="Masukkan nama pengguna">
-                              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                              @error('nama_penyewa')
-                              <div class="alert alert-danger mt-2">
-                                  {{ $message }}
-                              </div>
-                              @enderror
-                            </div>
-                            <div class="form-group">
-                              <label for="exampleInputPassword1">Alamat Customer</label>
-                              <input type="text" name="Alamat" class="form-control" placeholder="Masukkan alamat pengguna">
-                              @error('alamat')
-                              <div class="alert alert-danger mt-2">
-                                  {{ $message }}
-                              </div>
-                              @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlSelect1">No. Handphone</label>
-                                <input type="tel" name="Handphone" class="form-control" placeholder="Masukkan No HP">
-                                 @error('no_hp')
-                                 <div class="alert alert-danger mt-2">
-                                     {{ $message }}
-                                 </div>
-                                 @enderror
-                              </div>
-                              <br/>
                               <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                <label for="exampleInputPassword1">Alamat Customer</label>
+                                <input type="text" name="alamat" class="form-control" placeholder="Masukkan alamat pengguna">
+                                @error('alamat')
+                                <div class="alert alert-danger mt-2">
+                                    {{ $message }}
+                                </div>
+                                @enderror
                               </div>
+                              <div class="form-group">
+                                  <label for="exampleFormControlSelect1">No. Handphone</label>
+                                  <input type="tel" name="no_hp" class="form-control" placeholder="Masukkan No HP">
+                                   @error('no_hp')
+                                   <div class="alert alert-danger mt-2">
+                                       {{ $message }}
+                                   </div>
+                                   @enderror
+                                </div>
+                                <br/>
+                                <div class="form-group">
+                                  <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
                           </form>
            
                         
-                        {{-- {{ $penyewa->links() }} --}}
+                        {{-- {{ $customer->links() }} --}}
                     </div>
                 </div>
             </div>
