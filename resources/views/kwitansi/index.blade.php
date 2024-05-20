936555e93f6d8f2a88199c75b503f4259c9a4979
fiddymulyadi <232220021@unmuhpnk.ac.id>
2024-05-20 07:59:39 +0700

    Create index.blade.php

---
+++ resources/views/kwitansi/index.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: white">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Data Kwitansi</h3>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('kwitansi.create') }}" class="btn btn-md btn-info mb-3">TAMBAH</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Id Kwitansi</th>
+                                    <th scope="col">Tanggal Kwitansi</th>
+                                    <th scope="col" style="width: 20%">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($dataKwitansi as $index => $kwitansi)
+                                    <tr>
+                                        <td class="text-center">
+                                            {{ ++$index }}
+                                        </td>
+                                        <td>{{ $kwitansi->id_kwitansi }}</td>
+                                        <td>{{ $kwitansi->tanggal_kwitansi }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('kwitansi.destroy', $kwitansi->id_kwitansi) }}" method="POST">
+                                                <a href="{{ route('kwitansi.show', $kwitansi->id_penyewa) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                                <a href="{{ route('kwitansi.edit', $kwitansi->id_penyewa) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data User Belum Ada.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{-- {{ $penyewa->links() }} --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
